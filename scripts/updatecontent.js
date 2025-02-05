e8f14d20cbd0f3a9bf0ab38df387a71936240b3e
arbinger <thomas.iniguez+github@free.fr>
2025-02-05 18:46:12 +0100

    Remove debug logging in updatecontent.js

--- scripts/updatecontent.js
+++ scripts/updatecontent.js
@@ -1,2 +1,2 @@
-console.log(hexo)
-hexo.extend.processor.register("preprocessor", (app) => {console.log(app)})
+/*console.log(hexo)
+hexo.extend.processor.register("preprocessor", (app) => {console.log(app)})*/

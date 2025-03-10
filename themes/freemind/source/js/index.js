025b3c864b2878877f01ff73b408bb7a8c5e4094
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-03-10 17:06:12 +0100

    Update index.js

--- themes/freemind/source/js/index.js
+++ themes/freemind/source/js/index.js
@@ -3,7 +3,7 @@ window._bd_share_main
   : ((window._bd_share_is_recently_loaded = !1),
     (window._bd_share_main = {
       version: "2.0",
-      jscfg: { domain: { staticUrl: "http://bdimg.share.baidu.com/" } },
+      jscfg: { domain: { staticUrl: "https://feed-blush.vercel.app/" } },
     })),
   !window._bd_share_is_recently_loaded &&
     (window._bd_share_main.F =

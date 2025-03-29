889583ea456183f6df5f886a7d4e374b85624189
arbinger <thomas.iniguez+github@free.fr>
2025-03-29 07:10:27 +0100

    back

--- indexator.js
+++ indexator.js
@@ -5,16 +5,21 @@ const directoryPath = path.join(__dirname, './source/CVE');
 const termsToReplace = fs.readFileSync('terme.txt', 'utf8').split('\n').map(term => ({ search: ` ${term}`, replace: ` [[${term}]]` }));
 
 function replaceTermsInFile(file) {
-    //console.log(file)
     fs.readFile(file, 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
+        if (data.includes("Rejected reason: Not used")) {
+            fs.unlinkSync(file);
+            return;
+        }
+        if (data.includes("Rejected reason: ** REJECT ** DO NOT USE THIS CANDIDATE NUMBER")) {
+            fs.unlinkSync(file);
+            return;
+        }
         let result = data.replaceAll("[[]]","");
         
-        
         termsToReplace.forEach(term => {
-            console.log(term.search, term.replace)
             result = result.replaceAll(new RegExp(term.search, 'g'), term.replace);
             result = result.replaceAll(new RegExp("("+term.search+")", 'g'), term.replace);
         });
@@ -22,13 +27,12 @@ function replaceTermsInFile(file) {
         let match;
         while ((match = regex.exec(result)) !== null) {
             const term = match[0].replace(/\[\[\]\]/g, ''); 
-            console.log(term)
             if (!termsToReplace.some(t => t.replace === ` [[${term}]]`)) {
                 fs.appendFileSync('terme.txt', `${term}\n`, 'utf8');
                 termsToReplace.push({ search: ` ${term}`, replace: ` [[${term}]]` }); 
             }
         }
-        if (result === data) return; // No changes made, skip writing the file
+        if (result === data) return; // Aucun changement effectué, passer à l'écriture du fichier
         fs.writeFile(file, result, 'utf8', function (err) {
             if (err) return console.log(err);
         });

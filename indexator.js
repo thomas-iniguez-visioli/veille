1f759e0a3a16a6abcd6f1d1e0ce9e4cebda11ab6
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-04-30 07:36:22 +0200

    Update indexator.js

--- indexator.js
+++ indexator.js
@@ -9,7 +9,7 @@ function replaceTermsInFile(file) {
         if (err) {
             return console.log(err);
         }
-        if (data.lowerCase().includes("rejected reason")) {
+        if (data.lowercase().includes("rejected reason")) {
             fs.unlinkSync(file);
             return;
         }

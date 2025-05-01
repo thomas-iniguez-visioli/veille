e94f0962960f3d158bca416aebca99b1cbd92304
arbinger <thomas.iniguez+github@free.fr>
2025-05-01 10:04:37 +0200

    e

--- indexator.js
+++ indexator.js
@@ -33,6 +33,7 @@ function replaceTermsInFile(file) {
             }
         }
         if (result === data) return; // Aucun changement effectué, passer à l'écriture du fichier
+        delete data
         fs.writeFile(file, result.replaceAll("[[]]",""), 'utf8', function (err) {
             if (err) return console.log(err);
         });
